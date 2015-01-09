1c1b0d6992bb4439359512cafedfe37c53c93b86
DjinnS <djinns@chninkel.net>
2015-01-09 21:23:25 +0100

    RBL lists

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>RBL Lists</h1>
+
+The goal of this project is to release the most used RBL in differents formats to help people to build efficient scripts.
---
+++ rbl.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+#
+# rbl.py
+#
+# List of the most used RBL in a python format.
+#
+# Use this file in your script. For example, use the raw version from github.
+#
+# Also, you can use the others versions depend of the language you want to use.
+#
+# http://github.com/djinns/rbl-lists
+#
+# Copyright (C) 2012 djinns@chninkel.net
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+rbl = [
+	'b.barracudacentral.org',
+	'cbl.abuseat.org',
+	'http.dnsbl.sorbs.net',
+	'misc.dnsbl.sorbs.net',
+	'socks.dnsbl.sorbs.net',
+	'web.dnsbl.sorbs.net',
+	'dnsbl-1.uceprotect.net',
+	'dnsbl-3.uceprotect.net',
+	'sbl.spamhaus.org',
+	'zen.spamhaus.org',
+	'psbl.surriel.com',
+	'dnsbl.njabl.org',
+	'rbl.spamlab.com',
+	'noptr.spamrats.com',
+	'cbl.anti-spam.org.cn',
+	'dnsbl.inps.de',
+	'httpbl.abuse.ch',
+	'korea.services.net',
+	'virus.rbl.jp',
+	'wormrbl.imp.ch',
+	'rbl.suresupport.com',
+	'ips.backscatterer.org',
+	'opm.tornevall.org',
+	'multi.surbl.org',
+	'tor.dan.me.uk',
+	'relays.mail-abuse.org',
+	'rbl-plus.mail-abuse.org',
+	'access.redhawk.org',
+	'rbl.interserver.net',
+	'bogons.cymru.com',
+	'bl.spamcop.net',
+	'dnsbl.sorbs.net',
+	'dul.dnsbl.sorbs.net',
+	'smtp.dnsbl.sorbs.net',
+	'spam.dnsbl.sorbs.net',
+	'zombie.dnsbl.sorbs.net',
+	'dnsbl-2.uceprotect.net',
+	'pbl.spamhaus.org',
+	'xbl.spamhaus.org',
+	'bl.spamcannibal.org',
+	'ubl.unsubscore.com',
+	'combined.njabl.org',
+	'dyna.spamrats.com',
+	'spam.spamrats.com',
+	'cdl.anti-spam.org.cn',
+	'drone.abuse.ch',
+	'dul.ru',
+	'short.rbl.jp',
+	'spamrbl.imp.ch',
+	'virbl.bit.nl',
+	'dsn.rfc-ignorant.org',
+	'dsn.rfc-ignorant.org',
+	'netblock.pedantic.org',
+	'ix.dnsbl.manitu.net',
+	'rbl.efnetrbl.org',
+	'blackholes.mail-abuse.org',
+	'dnsbl.dronebl.org',
+	'db.wpbl.info',
+	'query.senderbase.org',
+	'bl.emailbasura.org',
+	'combined.rbl.msrbl.net',
+	'multi.uribl.com',
+	'black.uribl.com',
+	'cblless.anti-spam.org.cn',
+	'cblplus.anti-spam.org.cn',
+	'blackholes.five-ten-sg.com',
+	'sorbs.dnsbl.net.au',
+	'rmst.dnsbl.net.au',
+	'dnsbl.kempt.net',
+	'blacklist.woody.ch',
+	'rot.blackhole.cantv.net',
+	'virus.rbl.msrbl.net',
+	'phishing.rbl.msrbl.net',
+	'images.rbl.msrbl.net',
+	'spam.rbl.msrbl.net',
+	'spamlist.or.kr',
+	'dnsbl.abuse.ch',
+	'bl.deadbeef.com',
+	'ricn.dnsbl.net.au',
+	'forbidden.icm.edu.pl',
+	'probes.dnsbl.net.au',
+	'ubl.lashback.com',
+	'ksi.dnsbl.net.au',
+	'uribl.swinog.ch',
+	'bsb.spamlookup.net',
+	'dob.sibl.support-intelligence.net',
+	'url.rbl.jp',
+	'dyndns.rbl.jp',
+	'omrs.dnsbl.net.au',
+	'osrs.dnsbl.net.au',
+	'orvedb.aupads.org',
+	'relays.nether.net',
+	'relays.bl.gweep.ca',
+	'relays.bl.kundenserver.de',
+	'dialups.mail-abuse.org',
+	'rdts.dnsbl.net.au',
+	'duinv.aupads.org',
+	'dynablock.sorbs.net',
+	'residential.block.transip.nl',
+	'dynip.rothen.com',
+	'dul.blackhole.cantv.net',
+	'mail.people.it',
+	'blacklist.sci.kun.nl',
+	'all.spamblock.unit.liu.se',
+	'spamguard.leadmon.net',
+	'csi.cloudmark.com'
+]
